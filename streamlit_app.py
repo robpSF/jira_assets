3ec4af5ff006bf4ae1fbf4995908fee3123320ee
robpSF <robert@tstoryteller.com>
2023-04-16 18:55:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,15 +5,17 @@ import streamlit as st
 import pandas as pd
 import base64
 
-import numpy as np
+import plotly.graph_objects as go
+
+#import numpy as np
+#import plotly.express as px
+#import plotly.figure_factory as ff
+#import plotly.graph_objects as go
 
 def create_download_link(val, filename):
     b64 = base64.b64encode(val)  # val looks like b'...'
     return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
 
-
-def get_controls_list():
-    return
 
 def read_data_files(processes,assets,threats,controls):
     try:
@@ -43,6 +45,61 @@ def get_matching_controls(df_controls, df_matching_threats):
     new_df = matching_controls[['Name', 'Description', "Confluence page","Key"]].set_index('Name')
     return new_df
 
+def print_control_count(df_controls, df_matching_threats,charts):
+    data_controls = pd.concat([df_matching_threats["Control_1"], df_matching_threats["Control_2"],
+                               df_matching_threats["Control_3"], df_matching_threats["Control_4"],
+                               df_matching_threats["Control_5"],
+                               df_matching_threats["Control_6"], df_matching_threats["Control_7"]])
+    data_controls_list = data_controls.dropna().tolist()
+    # convert to pandas DataFrame
+    df_data_controls_list = pd.DataFrame(data_controls_list, columns=['Key'])
+    # count the occurrence of each unique control
+    df_control_count = df_data_controls_list['Key'].value_counts().reset_index()
+    # rename columns
+    df_control_count.columns = ['Key', 'Count']
+
+
+    #st.write(df_controls)
+    #add description
+    # merge the two dataframes based on the common column "control"
+    df_control_count = pd.merge(df_control_count, df_controls, left_on='Key', right_on='Key')
+    #df_control_count =df_control_count.drop(columns=["Key"])
+
+    #st.write(data_controls_list)
+    st.write("Significance of control")
+    st.write(df_control_count)
+
+    if charts:
+        df_control_count = df_control_count.drop(["Description","Confluence page"], axis=1)
+        fig = go.Figure(data=[go.Table(
+            header=dict(values=list(df_control_count.columns),
+                        fill_color='paleturquoise',
+                        align='left'),
+            columnwidth=[1, 1,6,2],
+            cells=dict(values=[df_control_count[col] for col in df_control_count.columns],
+                       fill_color='lavender',
+                       align='left')) ])
+
+        fig.update_layout(height=len(df_control_count)*40,width=1000)
+        st.plotly_chart(fig)
+
+    return
+
+def print_threats_as_chart(df_threats):
+    df_threats = df_threats.reset_index().rename(columns={'index': 'Threat'})
+    fig = go.Figure(data=[go.Table(
+        header=dict(values=list(df_threats.columns),
+                    fill_color='paleturquoise',
+                    align='left'),
+        columnwidth=[2,1,1,1,6],
+        cells=dict(values=[df_threats[col] for col in df_threats.columns],
+                   fill_color='lavender',
+                   align='left')) ])
+
+    fig.update_layout(height=len(df_threats)*90,width=1000)
+    st.plotly_chart(fig)
+    return
+
 def print_controls(df_matching_controls):
     for index, row in df_matching_controls.iterrows():
         try:
@@ -58,8 +115,24 @@ def print_controls(df_matching_controls):
             st.write(confluence_page)
     return
 
+def print_the_threats_and_controls(df_controls,df_filtered_threats,charts):
+
+    df_controls_for_filtered_threats = get_matching_controls(df_controls, df_filtered_threats)
+
+    #print popularity/importance of controls
+    print_control_count(df_controls, df_filtered_threats, charts)
+    #st.write(df_controls_for_filtered_threats)
+
+    for index, row in df_filtered_threats.iterrows():
+        name = index
+        score = str(row['Score'])
+        st.markdown("### " + name + " (score="+score+")")
+        df_controls_for_this_row = get_matching_controls(df_controls,pd.DataFrame([row]))
+        print_controls(df_controls_for_this_row)
+    return
+
 #show details by selected business process
-def business_process(processes,assets,threats,controls):
+def business_process(processes,assets,threats,controls,charts):
     df_processes, df_assets, df_threats, df_controls = read_data_files(processes, assets, threats, controls)
 
     # create a list of the business processes
@@ -114,45 +187,20 @@ def business_process(processes,assets,threats,controls):
         matching_threats = df_threats[df_threats["Key"].isin(data_threat_list)]
         matching_threats.sort_values(by='Name', inplace=True)
 
-        new_df = matching_threats[['Name', 'Likelihood', "Impact", "Score", "Rationale"]].set_index('Name')
+        new_df = matching_threats[['Name', 'Likelihood', "Impact", "Score", "Rationale"]].set_index('Name').sort_values(by='Score', ascending=False)
         st.table(new_df)
 
-        # fig = px.bar(matching_threats, y=matching_threats.Name, x=matching_threats.Score,orientation='h',
-        #             labels={'y': 'Threat', 'x': 'Score'})
-        # st.plotly_chart(fig)
+        #if charts:
+        #    print_threats_as_chart(new_df)
 
-        #########################
-        # controls
-        ########################
-        data_controls = pd.concat([matching_threats["Control_1"], matching_threats["Control_2"],
-                                   matching_threats["Control_3"], matching_threats["Control_4"],
-                                   matching_threats["Control_5"],
-                                   matching_threats["Control_6"], matching_threats["Control_7"]])
-        data_controls_list = data_controls.dropna().tolist()
 
-        # remove any duplicates
-        data_controls_list = list(set(data_controls_list))
 
-        st.subheader("Controls")
-        # st.write(data_controls_list)
+        df_filtered_threats = matching_threats[
+            ['Name', 'Likelihood', "Impact", "Score", "Rationale", "Key", "Control_1", "Control_2", "Control_3",
+             "Control_4", "Control_5", "Control_6", "Control_7"]].set_index('Name')
 
-        # create the dataframe
-        matching_controls = df_controls[df_controls["Key"].isin(data_controls_list)]
-        matching_controls.sort_values(by='Name', inplace=True)
+        print_the_threats_and_controls(df_controls, df_filtered_threats,charts)
 
-        new_df = matching_controls[['Name', 'Description', "Confluence page"]].set_index('Name')
-        # st.dataframe(new_df, height=1200)
-
-        for index, row in matching_controls.iterrows():
-            name = row['Name']
-            description = row['Description']
-            confluence_page = row['Confluence page']
-            st.markdown("**" + name + "**")
-            if not pd.isna(description):
-                st.markdown("_" + description + "_")
-            if not pd.isna(confluence_page):
-                st.write(confluence_page)
-            # st.markdown("___")
 
     #  export_as_pdf = True #st.button("Export Report")
 
@@ -167,7 +215,7 @@ def business_process(processes,assets,threats,controls):
     return
 
 # provide overview of all controls and threats
-def overview(processes,assets,threats,controls):
+def overview(processes,assets,threats,controls,charts):
     df_processes, df_assets, df_threats, df_controls = read_data_files(processes, assets, threats, controls)
 
     # print things that don't look right or need improvement
@@ -205,17 +253,9 @@ def overview(processes,assets,threats,controls):
     df_filtered_threats = df_threats[df_threats['Score'] >= cutoff]
     df_filtered_threats = df_filtered_threats[['Name', 'Likelihood', "Impact", "Score", "Rationale", "Key","Control_1","Control_2","Control_3","Control_4","Control_5","Control_6","Control_7"]].set_index(
         'Name').sort_values(by='Score', ascending=False)
+
     st.write(df_filtered_threats)
-
-    df_controls_for_filtered_threats = get_matching_controls(df_controls, df_filtered_threats)
-    #st.write(df_controls_for_filtered_threats)
-
-    for index, row in df_filtered_threats.iterrows():
-        name = index
-        score = str(row['Score'])
-        st.markdown("### " + name + " (score="+score+")")
-        df_controls_for_this_row = get_matching_controls(df_controls,pd.DataFrame([row]))
-        print_controls(df_controls_for_this_row)
+    print_the_threats_and_controls(df_controls,df_filtered_threats, charts)
 
 
     #st.write("Highest scoring threats (those equal to and above "+threat_cutoff+")")
@@ -247,10 +287,11 @@ report_text = ""
 #
 # -------------------
 
-main_option = st.selectbox("Select analysis required", ["Business Area","Overview"])
+main_option = st.radio("Select analysis required", ["Business Area","Overview"])
+charts = st.radio("Show tables as graphic", ["yes","no"])=="yes"
 
 if main_option == "Business Area":
-    business_process(processes,assets,threats,controls)
+    business_process(processes,assets,threats,controls,charts)
 if main_option == "Overview":
-    overview(processes,assets,threats,controls)
+    overview(processes,assets,threats,controls,charts)
 
